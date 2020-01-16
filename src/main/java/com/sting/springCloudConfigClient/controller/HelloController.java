920d683ab2abcc6bbd7c83f1698b08add10a3e5c
shiqingshan <sqskg@163.com>
2020-01-16 14:11:02 +0800

    add refensh futrue

--- src/main/java/com/sting/springCloudConfigClient/controller/HelloController.java
+++ src/main/java/com/sting/springCloudConfigClient/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.sting.springCloudConfigClient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${info.profile:dev}")
